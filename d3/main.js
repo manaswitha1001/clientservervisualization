3df834c42abafc4e2b5c4902bcaea8138c489848
Lakshmimanaswitha <chimakurthi.l@husky.neu.edu>
2019-03-13 12:11:28 -0400

    First commit Message

--- d3/main.js
+++ d3/main.js
@@ -13,91 +13,76 @@ function my() {
     
     d3.json(dataurl).then(function(data) {
 
-	   document.getElementById('demo').innerHTML = "";
-
-     
-     //console.log(data);
-
-	  generateplot(JSON.parse(data));
-    });
-}
 
 
-
-function generateplot(data){
-
-
-var margin = {top: 50, right: 50, bottom: 50, left: 50}, width = 600- margin.left - margin.right , height = 1000 - margin.top - margin.bottom; // Use the window's height
+	   console.log(data)
 
 
-// 5. X scale will use the index of our data
-var xScale = d3.scaleLinear()
-    .domain([0, 1]) // input
-    .range([0, width]); // output
-
-// 6. Y scale will use the randomly generate number 
-var yScale = d3.scaleLinear()
-    .domain([0, 1]) // input 
-    .range([height, 0]); // output 
-
-// 7. d3's line generator
-var line = d3.line()
-    .x(function(d) { return xScale(d.fpr); }) // set the x values for the line generator
-    .y(function(d) { return yScale(d.tpr); }) // set the y values for the line generator 
-    //.curve(d3.curveMonotoneX) // apply smoothing to the line
-
-// 8. An array of objects of length N. Each object has key -> value pair, the key being "y" and the value is a random number
-
-// 1. Add the SVG to the page and employ #2
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-// 3. Call the x axis in a group tag
-svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(xScale)); // Create an axis component with d3.axisBottom
-
-// 4. Call the y axis in a group tag
-svg.append("g")
-    .attr("class", "y axis")
-    .call(d3.axisLeft(yScale)); // Create an axis component with d3.axisLeft
-
-// 9. Append the path, bind the data, and call the line generator 
-svg.append("path")
-    .datum(data) // 10. Binds data to the line 
-    .attr("class", "line") // Assign a class for styling 
-    .attr("d", line)
-    .attr("fill", "none")
-            .attr("stroke", "green")
-            .attr("stroke-width", "3px"); // 11. Calls the line generator 
+  var margin = {top: 50, right: 50, bottom: 50, left: 50}, width = 600- margin.left - margin.right , height = 1000 - margin.top - margin.bottom; // Use the window's height
 
 
-//
-svg.append("text")
-       .attr("transform",
-             "translate(" + (width / 2) + " ," + 0 + ")")
-       .attr("text-anchor", "middle")
-       .text("ROC Curve");
+  
+  var xScale = d3.scaleLinear()
+      .domain([0, 1]) 
+      .range([0, width]); 
 
-    // Add the text label for the x axis
-    svg.append("text")
-       .attr("transform",
-             "translate(" + (width / 2) + " ," + (height + margin.bottom - 15) + ")")
-       .attr("text-anchor", "middle")
-       .text("False positive rate");
-    
-    // Add the text label for the y axis
-    svg.append("text")
-       .attr("transform", "rotate(-90)")
-       .attr("y", 0 - margin.left + 15)
-       .attr("x", 0 - (height / 2))
-       .attr("text-anchor", "middle")
-       .text("True positive rate");
+  
+  var yScale = d3.scaleLinear()
+      .domain([0, 1]) 
+      .range([height, 0]); 
+
+  
+  var line = d3.line()
+      .x(function(d) { return xScale(d.fpr); }) 
+      .y(function(d) { return yScale(d.tpr); }) 
+  var svg = d3.select("body").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(xScale)); 
+
+  
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(d3.axisLeft(yScale)); 
+  svg.append("path")
+      .datum(data) 
+      .attr("class", "line") 
+      .attr("d", line)
+      .attr("fill", "none")
+              .attr("stroke", "green")
+              .attr("stroke-width", "3px"); 
 
 
 
+  svg.append("text")
+         .attr("transform",
+               "translate(" + (width / 2) + " ," + 0 + ")")
+         .attr("text-anchor", "middle")
+         .text("ROC Curve");
+
+ 
+      svg.append("text")
+         .attr("transform",
+               "translate(" + (width / 2) + " ," + (height + margin.bottom - 15) + ")")
+         .attr("text-anchor", "middle")
+         .text("False positive rate");
+      
+  
+      svg.append("text")
+         .attr("transform", "rotate(-90)")
+         .attr("y", 0 - margin.left + 15)
+         .attr("x", 0 - (height / 2))
+         .attr("text-anchor", "middle")
+         .text("True positive rate");
+
+
+
+     });
    }
